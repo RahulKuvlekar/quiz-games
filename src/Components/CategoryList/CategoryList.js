e82aeb1f41f5ffec2bc223d69fa18b72e7122bd6
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-05-28 00:41:26 +0530

    Feat- Added Category List Component

---
+++ src/Components/CategoryList/CategoryList.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import CategoryCard from "../CategoryCard/CategoryCard";
+import "./CategoryList.css";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../Firebase/firebase";
+
+const CategoryList = () => {
+  const [categoryData, setCategoryData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const categoryRef = collection(db, "Categories");
+      const categorySnap = await getDocs(categoryRef);
+      const categoryData = categorySnap.docs.map((snapshot) => ({
+        id: snapshot.id,
+        ...snapshot.data(),
+      }));
+
+      setCategoryData(categoryData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="categoryList-section">
+      {categoryData.length > 0 &&
+        categoryData.map((category, idx) => (
+          <CategoryCard
+            key={category?.id}
+            categoryData={category}
+            reverse={idx % 2 === 0 ? false : true}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default CategoryList;
