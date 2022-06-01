f6eff95c8f319ca8df158f6dde30ff153367764d
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-06-01 16:50:41 +0530

    Refactor - made clean code

--- src/Components/CategoryCard/CategoryCard.js
+++ src/Components/CategoryCard/CategoryCard.js
@@ -1,19 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./CategoryCard.css";
 
-const CategoryCard = ({
-  categoryData: { categoryName, categoryPoster, categoryDescription },
-  reverse = false,
-}) => {
+const CategoryCard = ({ categoryData, reverse = false }) => {
+  const { categoryName, categoryPoster, categoryDescription, id } =
+    categoryData;
+
+  const navigate = useNavigate();
+
+  const letExploreBtn = () => navigate(`category/${id}`);
+
   return (
     <div
       className={`categoryCard-section ${reverse ? "category-reverse" : null}`}
     >
-      <img className="categoryCard-img" src={categoryPoster} alt="marvelCard" />
+      <img
+        className="categoryCard-img"
+        src={categoryPoster}
+        alt={categoryName}
+      />
       <div className="categoryCard-description">
         <h1>{categoryName}</h1>
         <h3>{categoryDescription}</h3>
-        <button className="btn btn-primary">Lets Explore</button>
+        <button className="btn btn-primary" onClick={letExploreBtn}>
+          Lets Explore
+        </button>
       </div>
     </div>
   );
