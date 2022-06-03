838090c2e175d4d2a50c8bee86845c2b413d8df4
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-06-04 01:38:35 +0530

    Feat- Added Custom Toast Component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ToastProvider } from "./Context/ToastContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
