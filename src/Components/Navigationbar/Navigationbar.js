2434031bcd338b0df9a359da1635cc71fa8db5a4
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-06-04 01:43:01 +0530

    Update - Added Logout button & default avatar

--- src/Components/Navigationbar/Navigationbar.js
+++ src/Components/Navigationbar/Navigationbar.js
@@ -1,9 +1,34 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import "./Navigationbar.css";
 import { menus } from "./menus";
+import { useAuthContext } from "../../Hooks/useAuthContext";
+import { signOutUser } from "../../Utils/authentication";
+import { useToastContext } from "../../Hooks/useToastContext";
+import { ADD_TOAST, DANGER, SUCCESS } from "../../Constant/constant";
+import { createToast } from "../../Utils/createToast";
 
 const Navigationbar = () => {
+  const { isAuthenticated } = useAuthContext();
+  const { dispatchToast } = useToastContext();
+  const navigate = useNavigate();
+
+  const signOutHandler = () => {
+    try {
+      signOutUser();
+      dispatchToast({
+        type: ADD_TOAST,
+        payload: createToast(SUCCESS, "Logout Successfully 🎉"),
+      });
+      navigate("/");
+    } catch (error) {
+      dispatchToast({
+        type: ADD_TOAST,
+        payload: createToast(DANGER, error.message),
+      });
+    }
+  };
+
   return (
     <header className="navigation-bar">
       <div className="nav-logo-title">
@@ -12,7 +37,8 @@ const Navigationbar = () => {
         </Link>
       </div>
       <ul className="nav-pill nav-menu">
-        {menus &&
+        {!isAuthenticated &&
+          menus &&
           menus.map(({ name, pathname }) => (
             <li className="list-inline-item" key={`navigationbar-menu-${name}`}>
               <NavLink
@@ -24,13 +50,16 @@ const Navigationbar = () => {
             </li>
           ))}
         {/* If Login Avatar displays */}
-        {/* <li>
-          <img
-            src="/Images/avatar.png"
-            className="avatar avatar-md"
-            alt="loginAvatar"
-          />
-        </li> */}
+        {isAuthenticated && (
+          <li>
+            <img
+              src="/Images/avatarDefault.svg"
+              className="avatar avatar-md"
+              alt="loginAvatar"
+              onClick={signOutHandler}
+            />
+          </li>
+        )}
       </ul>
     </header>
   );
