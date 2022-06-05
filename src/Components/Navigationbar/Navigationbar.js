d0580253596dc05af9da4475cab2728433175ddb
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-06-06 00:39:21 +0530

    Update - Added new menus

--- src/Components/Navigationbar/Navigationbar.js
+++ src/Components/Navigationbar/Navigationbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import "./Navigationbar.css";
-import { menus } from "./menus";
+import { menus, mainMenus } from "./menus";
 import { useAuthContext } from "../../Hooks/useAuthContext";
 import { signOutUser } from "../../Utils/authentication";
 import { useToastContext } from "../../Hooks/useToastContext";
@@ -9,7 +9,7 @@ import { ADD_TOAST, DANGER, SUCCESS } from "../../Constant/constant";
 import { createToast } from "../../Utils/createToast";
 
 const Navigationbar = () => {
-  const { isAuthenticated } = useAuthContext();
+  const { user, isAuthenticated } = useAuthContext();
   const { dispatchToast } = useToastContext();
   const navigate = useNavigate();
 
@@ -18,7 +18,12 @@ const Navigationbar = () => {
       signOutUser();
       dispatchToast({
         type: ADD_TOAST,
-        payload: createToast(SUCCESS, "Logout Successfully 🎉"),
+        payload: createToast(
+          SUCCESS,
+          `${
+            user?.displayName !== null ? user?.displayName : ""
+          } Logout Successfully 🎉`
+        ),
       });
       navigate("/");
     } catch (error) {
@@ -31,10 +36,28 @@ const Navigationbar = () => {
 
   return (
     <header className="navigation-bar">
-      <div className="nav-logo-title">
-        <Link className="link-no-style" to="/">
-          QuizGames
-        </Link>
+      <div className="nav-mainMenu">
+        <div className="nav-logo-title">
+          <Link className="link-no-style app-name" to="/">
+            QuizGames
+          </Link>
+        </div>
+        <ul className="nav-pill nav-btn-icons">
+          {mainMenus &&
+            mainMenus.map(({ name, pathname }) => (
+              <li
+                className="list-inline-item"
+                key={`navigationbar-mainMenu-${name}`}
+              >
+                <NavLink
+                  to={pathname}
+                  className={({ isActive }) => (isActive ? "nav-active" : "")}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            ))}
+        </ul>
       </div>
       <ul className="nav-pill nav-menu">
         {!isAuthenticated &&
